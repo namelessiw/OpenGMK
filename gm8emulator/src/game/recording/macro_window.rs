ba8c206f2e96a926088b248c9022fab4c45d58d5
Daggerfall-Is-The-Best-TES-Game <Davidcfried@gmail.com>
2023-10-14 11:41:16 -0400

    make sure directional inputs from prior room don't affect the current one

--- gm8emulator/src/game/recording/macro_window.rs
+++ gm8emulator/src/game/recording/macro_window.rs
@@ -125,6 +125,9 @@ impl MacroWindow {
                         let room_macro_bytes = room_macro.into_bytes();
                         self.input_buffer.fill(0);
                         self.input_buffer[..room_macro_bytes.len()].copy_from_slice(&room_macro_bytes);
+                        //makes sure directional inputs from prior room don't affect the current one
+                        keyboard_state.get_mut(Button::LeftArrow as usize).unwrap().reset_to_state(KeyState::Neutral);
+                        keyboard_state.get_mut(Button::RightArrow as usize).unwrap().reset_to_state(KeyState::Neutral);
                     },
                     Err(e) => println!("{}", e.to_string()),
                 }
